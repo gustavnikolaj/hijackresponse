ebd7a48e756b96b26d2f22e3aa726ce1229586e6
Gustav Nikolaj Olsen <gno@one.com>
2015-09-20 18:49:38 +0200

    somewhat working

--- poc.js
+++ poc.js
@@ -2,10 +2,33 @@ var Writable = require('stream').Writable
 var Readable = require('stream').Readable
 var Transform = require('stream').Transform
 
+var debug = require('debug')('hijack:debug')
+var info = require('debug')('hijack:info')
+var log = require('debug')('hijack:log')
+
 var Stream = require('stream').Stream
 
 var convertToUpperCaseTransformStream = new Transform()
+convertToUpperCaseTransformStream.count = 0
 convertToUpperCaseTransformStream._transform = function (chunk, encoding, cb) {
+  if (convertToUpperCaseTransformStream.count === 0) {
+    convertToUpperCaseTransformStream.count += 1
+    return setTimeout(function () {
+      if (encoding === 'buffer') {
+        var result = []
+        for (var i = 0; i < chunk.length; ++i) {
+          var charCode = chunk[i]
+          if (charCode > 96 && charCode < 123) {
+            charCode = charCode - 32
+          }
+          result.push(charCode)
+        }
+        this.push(new Buffer(result))
+        return cb()
+      }
+      return cb(new Error('chunk not a buffer!'))
+    }.bind(this), 1000)
+  }
   if (encoding === 'buffer') {
     var result = []
     for (var i = 0; i < chunk.length; ++i) {
@@ -41,30 +64,48 @@ function hijack (res, cb) {
   var _readReady = false
   var writeBuffer = []
 
-  hijackedRes._read = function (size) {
-    _readReady = true
+  function maybeWrite (chunk, writeBuffer) {
+    maybeNot = _readReady ? 'not ' : ''
+    debug('hijacked data from res. ' + maybeNot + 'buffering')
+      if (_readReady) {
+        _readReady = hijackedRes.push(chunk)
+        return true
+      } else {
+        writeBuffer.push(chunk)
+        return false
+      }
   }
 
-  res.write = function (chunk, encoding) {
+  function emptyWriteBuffer () {
     if (_readReady && writeBuffer.length > 0) {
       var newWriteBuffer = []
-      writeBuffer.forEach(function (obj) {
-        if (_readReady) {
-          _readReady = hijackedRes.push(obj.chunk)
-        } else {
-          newWriteBuffer.push(obj)
-        }
+      writeBuffer.forEach(function (chunk) {
+        maybeWrite(chunk, newWriteBuffer)
       })
       writeBuffer = newWriteBuffer
     }
-    if (_readReady) {
-      _readReady = hijackedRes.push(chunk)
-    } else {
-      writeBuffer.push({
-        chunk: chunk,
-        encoding: encoding
-      })
+  }
+
+  hijackedRes._read = function (size) {
+    debug('_read called %s [from _readReady state %s]', size, _readReady)
+    _readReady = true
+
+    emptyWriteBuffer()
+    return
+  }
+
+  res.write = function (rawChunk, encoding) {
+    var chunk = rawChunk
+    if (rawChunk !== null && !Buffer.isBuffer(chunk) && encoding !== 'buffer') {
+      if (!encoding) {
+        chunk = new Buffer(rawChunk)
+      } else {
+        chunk = new Buffer(rawChunk, encoding)
+      }
     }
+    debug('res.write called with "%s"', rawChunk ? rawChunk.toString().replace('\n', '\\n') : 'null')
+    emptyWriteBuffer()
+    return maybeWrite(chunk, writeBuffer)
   }
 
   res.end = function (chunk, encoding) {
@@ -75,6 +116,7 @@ function hijack (res, cb) {
   }
 
   hijackedRes.write = function (chunk, encoding) {
+    debug('hijackedRes.write called')
     origWrite.call(origRes, chunk, encoding)
   }
 
@@ -98,11 +140,13 @@ res._write = function (chunk, encoding, next) {
   throw new Error('not a buffer!')
 }
 
-// hijack(res, function (err, res) {
-//   res
-//     .pipe(convertToUpperCaseTransformStream)
-//     .pipe(res)
-// })
+
+
+hijack(res, function (err, res) {
+  res
+    .pipe(convertToUpperCaseTransformStream)
+    .pipe(res)
+})
 
 hijack(res, function (err, res) {
   res.on('data', function (chunk) {
@@ -114,6 +158,17 @@ hijack(res, function (err, res) {
   })
 })
 
-res.write('foo')
+// var i = 0
+// while ((res.write('foo ') !== false ) || i < 10) {
+//   res.write('foo ')
+//   i++
+// }
+
 res.write('bar')
 res.end('\n')
+
+
+res.on('end', function () {
+  console.log('ended')
+  process.exit(0)
+})
