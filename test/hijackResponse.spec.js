2f0eb2dae86583f2873191e6f35bcb9c11a2a2f0
Gustav Nikolaj Olsen <gno@one.com>
2015-09-20 13:07:08 +0200

    add tests not using express
    Adapted from unexpected-express assertion 'to yield exchange' and
    modified to work on a single response, not taking the response
    into account.

--- test/hijackResponse.spec.js
+++ test/hijackResponse.spec.js
@@ -1,4 +1,38 @@
 /* global describe, it */
+var expect = require('./unexpected-with-plugins')
+var passError = require('passerror')
+var hijackResponse = require('../')
+
 describe('hijackResponse', function () {
-  it('should ...', function () {})
+  it('should be able to hijack a reponse and rewrite it', function () {
+    return expect(function (res, handleError) {
+      hijackResponse(res, passError(handleError, function (res) {
+        var chunks = []
+        res.on('data', function (chunk) {
+          chunks.push(chunk)
+        })
+        res.on('end', function () {
+          var result = Buffer.concat(chunks).toString('utf-8').toUpperCase()
+          res.write(result, 'utf-8')
+          res.end()
+        })
+      }))
+
+      res.setHeader('Content-Type', 'text/plain')
+      res.write('foo')
+      res.end()
+    }, 'to yield response', 'FOO')
+  })
+  it('should be able to pipe hijacked res into it self.', function () {
+    return expect(function (res, handleError) {
+      hijackResponse(res, passError(handleError, function (res) {
+        res.pipe(res)
+      }))
+
+      res.setHeader('Content-Type', 'text/plain')
+      res.write('foo')
+      res.write('bar')
+      res.end()
+    }, 'to yield response', 'foobar')
+  })
 })
