5452dfae5d5e5f7023015d3c9ff7aee885b87738
Gustav Nikolaj Olsen <gno@one.com>
2015-09-21 13:48:14 +0200

    add test for hijacking a hijacked response while piping

--- test/hijackResponse.spec.js
+++ test/hijackResponse.spec.js
@@ -60,4 +60,38 @@ describe('hijackResponse', function () {
       res.end()
     }, 'to yield response', 'foobarqux')
   })
+  it('should be able to hijack an already hijacked response when piping', function () {
+    function appendToStream(value) {
+      var Transform = require('stream').Transform
+      var appendTo = new Transform({})
+      appendTo._transform = function (chunk, encoding, cb) {
+        this.push(chunk)
+        cb()
+      }
+      appendTo._flush = function (cb) {
+        this.push(new Buffer(value))
+        cb()
+      }
+      return appendTo
+    }
+    return expect(function (res, handleError) {
+      hijackResponse(res, passError(handleError, function (res) {
+        hijackResponse(res, passError(handleError, function (res) {
+          res.pipe(appendToStream('qux')).pipe(res)
+        }))
+        res.pipe(appendToStream('baz')).pipe(res)
+      }))
+
+      res.setHeader('Content-Type', 'text/plain')
+
+      var num = 0
+      function tick () {
+        res.write('foo')
+        num += 1
+        if (num < 5) return setImmediate(tick)
+        res.end('bar')
+      }
+      tick()
+    }, 'to yield response', 'foofoofoofoofoobarbazqux')
+  })
 })
