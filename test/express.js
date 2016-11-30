9c32740bb3d86bd2b9ebc4d6a8e01388576e3b7a
Pablo Sichert <mail@pablosichert.de>
2016-11-30 12:03:10 +0100

    Add test case that makes sure close event handlers are preserved

--- test/express.js
+++ test/express.js
@@ -1,13 +1,16 @@
-/* global describe, it */
+/* global describe, it, before, after */
+var http = require('http')
 var expect = require('unexpected')
   .clone()
   .use(require('unexpected-express'))
+  .use(require('unexpected-sinon'))
   .addAssertion('to yield response', function (expect, subject, value) {
     return expect(subject, 'to yield exchange', {
       request: 'GET /',
       response: value
     })
   })
+var sinon = require('sinon')
 var express = require('express')
 var passError = require('passerror')
 var hijackResponse = require('../')
@@ -162,5 +165,66 @@ describe('Express Integration Tests', function () {
         response: { body: /^0{1999998}$/ }
       }
     )
+  })
+  describe('against a real server', function () {
+    before(function () {
+      var self = this
+      var spy = sinon.spy()
+
+      this.spy = spy
+
+      return new Promise(function (resolve) {
+        var app = express()
+          .use(function (req, res, next) {
+            res.on('close', function () {
+              spy()
+              self.resolve()
+            })
+
+            next()
+          })
+          .use(function (req, res, next) {
+            hijackResponse(res, passError(next, function (res) {
+              res.pipe(res)
+            }))
+
+            next()
+          })
+          .use(function (req, res) {
+            res.write('foo')
+          })
+
+        var server = http.Server(app)
+        server.listen(0, function () {
+          resolve(server)
+        })
+      }).then(function (server) {
+        self.server = server
+      })
+    })
+    it('should not prevent "close" events registered on res from firing when hijacking', function () {
+      var self = this
+      var port = this.server.address().port
+      var spy = this.spy
+
+      return new Promise(function (resolve) {
+        self.resolve = resolve
+
+        var options = {
+          port: port
+        }
+
+        var request = http.request(options, function (res) {
+          request.abort()
+        })
+
+        request.end()
+      }).then(function () {
+        expect(spy, 'was called')
+      })
+    })
+    after(function () {
+      this.server.close()
+    })
   })
 })
