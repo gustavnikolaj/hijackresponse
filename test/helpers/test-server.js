1594d86b1891d0977c458509793376d32d7a63f8
Gustav Nikolaj Olsen <gno@one.com>
2019-03-04 14:33:07 +0100

    Rewrite the tests that relied on poor mocking
    I ran the tests in three combinations:
    
    1. With both old and new tests
    2. Only with old tests
    3. Only with new tests
    
    The coverage numbers remained the same in all configurations.

---
+++ test/helpers/test-server.js
@@ -0,0 +1,56 @@
+const http = require("http");
+
+module.exports = function createTestServer(serverImplementation) {
+  const server = http.createServer(serverImplementation);
+  const listener = server.listen();
+
+  const close = () =>
+    new Promise(resolve => server.close(() => resolve("foo")));
+
+  const makeRequest = () =>
+    new Promise(resolve => {
+      const resolveAndClose = (err, value) =>
+        resolve(
+          close().then(() => {
+            if (err) {
+              throw err;
+            }
+            return value;
+          })
+        );
+
+      const req = http.get({ port: listener.address().port });
+
+      req.once("response", res => {
+        const value = { statusCode: res.statusCode, headers: res.headers };
+        const contentType = res.headers && res.headers["content-type"];
+        const chunks = [];
+        res
+          .on("data", chunk => {
+            chunks.push(chunk);
+          })
+          .on("end", function() {
+            const responseBody = Buffer.concat(chunks);
+            value.rawBody = responseBody;
+
+            if (contentType === "application/json") {
+              try {
+                const parsedBody = JSON.parse(responseBody.toString("utf-8"));
+                value.body = parsedBody;
+              } catch (e) {}
+            } else if (/^text\//.test(contentType)) {
+              value.body = responseBody.toString("utf-8");
+            }
+
+            return resolveAndClose(null, value);
+          })
+          .on("error", err => resolveAndClose(err));
+      });
+
+      req.on("error", err => resolveAndClose(err));
+
+      req.end();
+    });
+
+  return makeRequest;
+};
